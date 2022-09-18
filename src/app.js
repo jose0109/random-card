7c3ea8c30a3862370510ad5bf98c04cfae77dda7
jose0109 <martinezujd@gmail.com>
2022-09-18 00:25:40 +0000

    finished code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let cardSuits = ["♦", "♥", "♠", "♣"];
+let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".num");
+  let suits = document.querySelectorAll(".suit");
+  let indexNum = Math.floor(Math.random() * numeros.length);
+  let indexSuit = Math.floor(Math.random() * cardSuits.length);
+  console.log(indexSuit);
+
+  if (indexSuit <= 1) {
+    card.style.color = "red";
+    suits.forEach(element => (element.style.color = "red"));
+  } else {
+    card.style.color = "black";
+    suits.forEach(element => (element.style.color = "black"));
+  }
+
+  card.innerHTML = numeros[indexNum];
+  suits.forEach(element => (element.innerHTML = cardSuits[indexSuit]));
 };
